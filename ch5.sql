6dd4550d75c013de03975e4d3660e7443ecec522
AlexandrAnatoliev <per-1986@list.ru>
2025-08-14 15:03:45 +0300

    fix: study chapter 5

--- ch5.sql
+++ ch5.sql
@@ -611,3 +611,450 @@ ORDER BY  percent DESC;
 -- AROVA          |    0.34
 -- IKOVA          |    0.31
 
+
+-- МАССИВЫ
+
+
+-- ЗАДАЧА: В билете не указывается явно, в один он конец или туда и обратно. Но это можно определить, 
+-- сравнив первый пункт отправления с последним пунктом назначения.
+-- Для каждого билета выведите аэропорты отправления и назначения без учета пересадок и с указанием того,
+-- взят ли он в один конец или туда и обратно.
+
+-- С помощью array_agg сворачиваем список аэропортов поти следования в пассажиров
+-- В качестве аэропорта назначения примем средний элемент массива (путь туда = путь обратно)
+
+WITH t AS (
+  SELECT  ticket_no,
+          a,
+          a[1]                  departure,
+          a[cardinality(a)]     last_arrival,
+          a[cardinality(a)/2+1] middle
+  FROM (
+    SELECT  t.ticket_no,
+            array_agg(  f.departure_airport
+              ORDER BY  f.scheduled_departure) ||
+            (array_agg( f.arrival_airport
+              ORDER BY  f.scheduled_departure DESC)
+            )[1] AS a
+    FROM    tickets t 
+            JOIN ticket_flights tf 
+              ON tf.ticket_no = t.ticket_no
+            JOIN flights f 
+              ON f.flight_id = tf.flight_id
+    GROUP BY t.ticket_no
+  ) t
+)
+SELECT  t.ticket_no,
+        t.a,
+        t.departure,
+        CASE
+          WHEN t.departure = t.last_arrival
+            THEN t.middle
+          ELSE t.last_arrival
+        END arrival,
+        (t.departure = t.last_arrival) return_ticket
+FROM t;
+
+--   ticket_no   |               a               | departure | arrival | return_ticket 
+-----------------+-------------------------------+-----------+---------+---------------
+-- 0005432000987 | {CSY,SVO}                     | CSY       | SVO     | f
+-- 0005432000988 | {CSY,SVO}                     | CSY       | SVO     | f
+-- 0005432000989 | {CSY,SVO}                     | CSY       | SVO     | f
+-- 0005432000990 | {CSY,SVO}                     | CSY       | SVO     | f
+-- 0005432000991 | {CSY,SVO}                     | CSY       | SVO     | f
+-- 0005432000992 | {CSY,SVO}                     | CSY       | SVO     | f
+-- 0005432000993 | {CSY,SVO}                     | CSY       | SVO     | f
+-- 0005432000994 | {CSY,SVO}                     | CSY       | SVO     | f
+-- 0005432000995 | {CSY,SVO}                     | CSY       | SVO     | f
+-- 0005432000996 | {CSY,SVO}                     | CSY       | SVO     | f
+
+-- ЗАДАЧА: Найдите билеты, взятые туда и обратно, в которых путь "туда" не совадает с путем "обратно".
+
+WITH ticket_routes AS (
+  SELECT  t.ticket_no,
+          array_agg(  f.departure_airport 
+            ORDER BY  f.scheduled_departure
+          ) AS departure_airports,
+          array_agg(  f.arrival_airport 
+            ORDER BY  f.scheduled_departure
+          ) AS arrival_airports,
+          array_agg(  f.departure_airport 
+            ORDER BY  f.scheduled_departure) || 
+          (array_agg( f.arrival_airport 
+            ORDER BY  f.scheduled_departure DESC)
+          )[1] AS full_route
+  FROM    tickets t 
+          JOIN ticket_flights tf 
+            ON tf.ticket_no = t.ticket_no
+          JOIN flights f 
+            ON f.flight_id = tf.flight_id
+  GROUP BY t.ticket_no
+),
+route_analysis AS (
+  SELECT  ticket_no,
+          full_route[1] AS departure,
+          full_route[array_length(full_route, 1)] AS last_arrival,
+          (full_route[1] = full_route[array_length(full_route, 1)]) AS is_return_ticket,
+          departure_airports,
+          arrival_airports,
+          array(
+            SELECT arrival_airports[i] 
+            FROM generate_subscripts(arrival_airports, 1) AS i 
+            ORDER BY i DESC
+          ) AS reversed_arrival_airports
+  FROM    ticket_routes
+)
+SELECT    ticket_no,
+          departure AS initial_departure,
+          last_arrival AS final_arrival,
+          departure_airports,
+          arrival_airports
+FROM      route_analysis
+WHERE     is_return_ticket = true
+AND       departure_airports <> reversed_arrival_airports;
+
+--   ticket_no   | initial_departure | final_arrival |    departure_airports     |     arrival_airports      
+-----------------+-------------------+---------------+---------------------------+---------------------------
+-- 0005432394032 | SVO               | SVO           | {SVO,OVB,ABA,TOF}         | {OVB,ABA,TOF,SVO}
+-- 0005432394033 | SVO               | SVO           | {SVO,OVB,ABA,TOF}         | {OVB,ABA,TOF,SVO}
+-- 0005432394034 | SVO               | SVO           | {SVO,OVB,ABA,TOF}         | {OVB,ABA,TOF,SVO}
+-- 0005432394035 | SVO               | SVO           | {SVO,OVB,ABA,TOF}         | {OVB,ABA,TOF,SVO}
+-- 0005432394036 | SVO               | SVO           | {SVO,OVB,ABA,TOF}         | {OVB,ABA,TOF,SVO}
+-- 0005432394037 | SVO               | SVO           | {SVO,OVB,ABA,TOF}         | {OVB,ABA,TOF,SVO}
+-- 0005432394038 | SVO               | SVO           | {SVO,OVB,ABA,TOF}         | {OVB,ABA,TOF,SVO}
+-- 0005432394039 | SVO               | SVO           | {SVO,OVB,ABA,TOF}         | {OVB,ABA,TOF,SVO}
+-- 0005432394040 | SVO               | SVO           | {SVO,OVB,ABA,TOF}         | {OVB,ABA,TOF,SVO}
+-- 0005432394041 | SVO               | SVO           | {SVO,OVB,ABA,TOF}         | {OVB,ABA,TOF,SVO}
+
+-- Создает таблицу, в которой указывается маршрут по которому летит самолет для данного билета
+
+SELECT  t.ticket_no,
+        array_agg(  f.departure_airport 
+          ORDER BY  f.scheduled_departure
+        ) AS departure_airports,
+        array_agg(  f.arrival_airport 
+          ORDER BY  f.scheduled_departure
+        ) AS arrival_airports,
+        array_agg(  f.departure_airport 
+          ORDER BY  f.scheduled_departure) || 
+        (array_agg( f.arrival_airport 
+          ORDER BY  f.scheduled_departure DESC)
+        )[1] AS full_route
+FROM    tickets t 
+        JOIN ticket_flights tf 
+          ON tf.ticket_no = t.ticket_no
+        JOIN flights f 
+          ON f.flight_id = tf.flight_id
+GROUP BY t.ticket_no;
+
+--   ticket_no   |    departure_airports     |     arrival_airports      |          full_route           
+-----------------+---------------------------+---------------------------+-------------------------------
+-- 0005432000987 | {CSY}                     | {SVO}                     | {CSY,SVO}
+-- 0005432000988 | {CSY}                     | {SVO}                     | {CSY,SVO}
+-- 0005432000989 | {CSY}                     | {SVO}                     | {CSY,SVO}
+-- 0005432000990 | {CSY}                     | {SVO}                     | {CSY,SVO}
+-- 0005432000991 | {CSY}                     | {SVO}                     | {CSY,SVO}
+-- 0005432000992 | {CSY}                     | {SVO}                     | {CSY,SVO}
+-- 0005432000993 | {CSY}                     | {SVO}                     | {CSY,SVO}
+-- 0005432000994 | {CSY}                     | {SVO}                     | {CSY,SVO}
+-- 0005432000995 | {CSY}                     | {SVO}                     | {CSY,SVO}
+
+-- ЗАДАЧА: Найдите такие пары аэропортов, рейсы между которыми в одну и другую стороны отправляются по разным дням недели.
+
+WITH flight_days AS (
+  SELECT    departure_airport,
+            arrival_airport,
+            array_agg(
+              DISTINCT EXTRACT(DOW FROM scheduled_departure) 
+              ORDER BY EXTRACT(DOW FROM scheduled_departure)
+            ) AS departure_days,
+            array_agg(
+              DISTINCT EXTRACT(DOW FROM scheduled_arrival) 
+              ORDER BY EXTRACT(DOW FROM scheduled_arrival)
+            ) AS arrival_days
+  FROM      flights
+  GROUP BY  departure_airport, 
+            arrival_airport
+),
+airport_pairs AS (  -- соединяем направления A->B и B->A
+  SELECT    f1.departure_airport AS airport1,
+            f1.arrival_airport AS airport2,
+            f1.departure_days AS days_1_to_2,
+            f2.departure_days AS days_2_to_1
+  FROM      flight_days f1
+            JOIN flight_days f2 
+              ON f1.departure_airport = f2.arrival_airport 
+            AND f1.arrival_airport = f2.departure_airport
+  WHERE     f1.departure_airport < f1.arrival_airport  -- избегаем дублирования пар
+)
+SELECT      airport1,
+            airport2,
+            days_1_to_2,
+            days_2_to_1
+FROM        airport_pairs
+WHERE       days_1_to_2 <> days_2_to_1    -- выбираем только те пары, где дни недели отличаются
+ORDER BY    airport1, 
+            airport2;
+
+-- airport1 | airport2 | days_1_to_2 | days_2_to_1 
+------------+----------+-------------+-------------
+-- AAQ      | NOZ      | {2}         | {1}
+-- ABA      | DME      | {0,3}       | {2,6}
+-- ABA      | GRV      | {3}         | {2}
+-- ABA      | KYZ      | {2,6}       | {1,4}
+-- AER      | GOJ      | {1,4,6}     | {1,3,6}
+-- AER      | IAR      | {0}         | {3}
+-- AER      | IWA      | {0,3}       | {2,6}
+-- AER      | KGP      | {6}         | {1}
+-- AER      | KJA      | {0,3}       | {0,4}
+-- AER      | NOZ      | {3,6}       | {2,6}
+-- ARH      | TJM      | {0,2,4}     | {1,3,6}
+-- ARH      | TOF      | {0,3}       | {0,4}
+
+-- Для каждого направления (пары аэропортов) собираем дни недели, в которые выполняются рейсы
+
+SELECT    departure_airport,
+          arrival_airport,
+          array_agg(
+            DISTINCT EXTRACT(DOW FROM scheduled_departure) 
+            ORDER BY EXTRACT(DOW FROM scheduled_departure)
+          ) AS departure_days,
+          array_agg(
+            DISTINCT EXTRACT(DOW FROM scheduled_arrival) 
+            ORDER BY EXTRACT(DOW FROM scheduled_arrival)
+          ) AS arrival_days
+FROM      flights
+GROUP BY  departure_airport, 
+          arrival_airport;
+
+-- departure_airport | arrival_airport | departure_days  |  arrival_days   
+---------------------+-----------------+-----------------+-----------------
+-- AAQ               | EGO             | {0,1,2,3,4,5,6} | {0,1,2,3,4,5,6}
+-- AAQ               | NOZ             | {2}             | {2}
+-- AAQ               | SVO             | {0,1,2,3,4,5,6} | {0,1,2,3,4,5,6}
+-- ABA               | ARH             | {6}             | {6}
+-- ABA               | DME             | {0,3}           | {0,3}
+-- ABA               | GRV             | {3}             | {3}
+-- ABA               | KYZ             | {2,6}           | {2,6}
+-- ABA               | OVB             | {0,1,2,3,4,5,6} | {0,1,2,3,4,5,6}
+-- ABA               | TOF             | {0,1,2,3,4,5,6} | {0,1,2,3,4,5,6}
+-- AER               | EGO             | {0,1,2,3,4,5,6} | {0,1,2,3,4,5,6}
+
+-- Вариант из учебника
+
+SELECT  r1.departure_airport,
+        r1.arrival_airport,
+        r1.days_of_week dow,
+        r2.days_of_week dow_back
+FROM    routes r1
+        JOIN routes r2 
+          ON r1.arrival_airport = r2.departure_airport
+        AND r1.departure_airport = r2.arrival_airport
+WHERE   NOT (r1.days_of_week && r2.days_of_week);
+
+-- departure_airport | arrival_airport |   dow   | dow_back 
+---------------------+-----------------+---------+----------
+-- UIK               | SGC             | {6}     | {7}
+-- SGC               | UIK             | {7}     | {6}
+-- IWA               | AER             | {2,6}   | {3,7}
+-- AER               | IWA             | {3,7}   | {2,6}
+-- DME               | PKV             | {2,5,7} | {1,4,6}
+-- PKV               | DME             | {1,4,6} | {2,5,7}
+-- IAR               | TOF             | {7}     | {1}
+-- SVO               | VKT             | {3,6}   | {4,7}
+
+
+-- РЕКУРСИВНЫЕ ЗАПРОСЫ
+
+
+-- ЗАДАЧА: Как добраться из Усть-Кута (UKX) в Нерюнги (CNN) с минимальным числом пересадок
+-- и сколько времени придется провести в воздухе?
+
+-- т.о. необходимо найти кратчайший путь в графе, что делается рекурсивным запросом
+
+WITH RECURSIVE p( -- инициализация начальных значений
+  last_arrival,   -- последний достигнутый аэропорт
+  destination,    -- аэропорт назначения
+  hops,           -- массив посещенных аэропортов
+  flights,        -- массив номеров рейсов
+  flight_time,    -- общее время в пути
+  found           -- флаг достижения пути назначения
+) AS (
+  SELECT  a_from.airport_code,
+          a_to.airport_code,
+          array[a_from.airport_code],             -- инициализируем массив hops начальным аэропортом
+          array[]::char(6)[],
+          interval '0',                           -- время поляета = 0
+          a_from.airport_code = a_to.airport_code -- флаг = true, когда начальный и конечный аэропорт совпадают
+  FROM    airports a_from,
+          airports a_to
+  WHERE   a_from.airport_code = 'UKX'
+  AND     a_to.airport_code = 'CNN'
+  UNION ALL        -- рекурсивный поиск маршрута 
+  SELECT  r.arrival_airport,
+          p.destination,
+          (p.hops || r.arrival_airport)::char(3)[],
+          (p.flights || r.flight_no)::char(6)[],
+          p.flight_time + r.duration,
+          bool_or(r.arrival_airport = p.destination)
+            OVER ()
+  FROM    p 
+          JOIN routes r
+            ON r.departure_airport = p.last_arrival
+  WHERE   NOT r.arrival_airport = ANY(p.hops)
+  AND     NOT p.found
+)
+SELECT    hops,
+          flights,
+          flight_time
+FROM      p 
+WHERE     p.last_arrival = p.destination;
+
+--         hops          |            flights            | flight_time 
+-------------------------+-------------------------------+-------------
+-- {UKX,KJA,OVB,MJZ,CNN} | {PG0022,PG0206,PG0390,PG0035} | 10:25:00
+-- {UKX,KJA,OVB,MJZ,CNN} | {PG0022,PG0207,PG0390,PG0035} | 10:25:00
+-- {UKX,KJA,SVO,MJZ,CNN} | {PG0022,PG0548,PG0120,PG0035} | 15:40:00
+-- {UKX,KJA,OVB,MJZ,CNN} | {PG0022,PG0206,PG0390,PG0036} | 10:25:00
+-- {UKX,KJA,OVB,MJZ,CNN} | {PG0022,PG0207,PG0390,PG0036} | 10:25:00
+-- {UKX,KJA,SVO,MJZ,CNN} | {PG0022,PG0548,PG0120,PG0036} | 15:40:00
+-- {UKX,KJA,OVS,LED,CNN} | {PG0022,PG0689,PG0686,PG0245} | 14:15:00
+-- {UKX,KJA,SVO,LED,CNN} | {PG0022,PG0548,PG0472,PG0245} | 14:35:00
+-- {UKX,KJA,SVO,LED,CNN} | {PG0022,PG0548,PG0471,PG0245} | 14:35:00
+-- {UKX,KJA,SVO,LED,CNN} | {PG0022,PG0548,PG0470,PG0245} | 14:35:00
+-- {UKX,KJA,SVO,LED,CNN} | {PG0022,PG0548,PG0469,PG0245} | 14:35:00
+-- {UKX,KJA,SVO,LED,CNN} | {PG0022,PG0548,PG0468,PG0245} | 14:35:00
+
+-- ЗАДАЧА: Какое максимальное количество пересадок может потребоваться,
+-- чтобы добраться из одного любого аэропорта в любой другой?
+
+WITH RECURSIVE p(
+  departure,
+  last_arrival,
+  destination,
+  hops,
+  found
+) AS (
+  SELECT  a_from.airport_code,
+          a_from.airport_code,
+          a_to.airport_code,
+          array[a_from.airport_code],
+          a_from.airport_code = a_to.airport_code
+  FROM    airports a_from,
+          airports a_to
+  UNION ALL
+  SELECT  p.departure,
+          r.arrival_airport,
+          p.destination,
+          (p.hops || r.arrival_airport)::char(3)[],
+          bool_or(r.arrival_airport = p.destination)
+            OVER (PARTITION BY  p.departure,
+                                p.destination)
+  FROM    p 
+          JOIN routes r 
+            ON r.departure_airport = p.last_arrival
+  WHERE   NOT r.arrival_airport = ANY(p.hops)
+  AND     NOT p.found
+)
+SELECT  max(cardinality(hops)-1)
+FROM    p
+WHERE   p.last_arrival = p.destination;
+
+--         hops          |            flights            | flight_time 
+-------------------------+-------------------------------+-------------
+-- {UKX,KJA,OVB,MJZ,CNN} | {PG0022,PG0206,PG0390,PG0035} | 10:25:00
+-- {UKX,KJA,OVB,MJZ,CNN} | {PG0022,PG0207,PG0390,PG0035} | 10:25:00
+-- {UKX,KJA,SVO,MJZ,CNN} | {PG0022,PG0548,PG0120,PG0035} | 15:40:00
+-- {UKX,KJA,OVB,MJZ,CNN} | {PG0022,PG0206,PG0390,PG0036} | 10:25:00
+-- {UKX,KJA,OVB,MJZ,CNN} | {PG0022,PG0207,PG0390,PG0036} | 10:25:00
+-- {UKX,KJA,SVO,MJZ,CNN} | {PG0022,PG0548,PG0120,PG0036} | 15:40:00
+
+-- ЗАДАЧА: Найдите кратчайший путь, ведущий из Усть-Кута (UKX) в Нерюнги (CNN),
+-- с точки зрения чистого времени перелетов (игнорируя время пересадок)
+
+WITH RECURSIVE p(
+  last_arrival,
+  destination,
+  flights,
+  flight_time,
+  min_time 
+) AS (
+  SELECT  a_from.airport_code,
+          a_to.airport_code,
+          array[]::char(6)[],
+          interval '0',
+          NULL::interval
+  FROM    airports a_from,
+          airports a_to
+  WHERE   a_from.airport_code = 'UKX'
+  AND     a_to.airport_code = 'CNN'
+  UNION ALL
+  SELECT  r.arrival_airport,
+          p.destination,
+          (p.flights || r.flight_no)::char(6)[],
+          p.flight_time + r.duration,
+          least(
+            p.min_time,
+            min(p.flight_time + r.duration)
+            FILTER (
+              WHERE r.arrival_airport = p.destination
+            ) OVER ()
+          )
+  FROM    p 
+          JOIN routes r 
+            ON r.departure_airport = p.last_arrival
+  WHERE   p.flight_time + r.duration
+          < coalesce(
+              p.min_time,
+              INTERVAL '1 year'
+            )
+)
+CYCLE last_arrival SET is_cycle USING hops
+SELECT  hops,
+        flights,
+        flight_time
+FROM    (
+          SELECT  hops,
+                  flights,
+                  flight_time,
+                  min(min_time) OVER () min_time
+          FROM    p 
+          WHERE   p.last_arrival = p.destination
+        ) t
+WHERE   flight_time = min_time;
+
+--                 hops                  |               flights                | flight_time 
+-----------------------------------------+--------------------------------------+-------------
+-- {(UKX),(KJA),(NOZ),(OVB),(MJZ),(CNN)} | {PG0022,PG0352,PG0297,PG0390,PG0035} | 10:10:00
+-- {(UKX),(KJA),(NOZ),(OVB),(MJZ),(CNN)} | {PG0022,PG0351,PG0297,PG0390,PG0035} | 10:10:00
+-- {(UKX),(KJA),(NOZ),(OVB),(MJZ),(CNN)} | {PG0022,PG0352,PG0298,PG0390,PG0035} | 10:10:00
+-- {(UKX),(KJA),(NOZ),(OVB),(MJZ),(CNN)} | {PG0022,PG0351,PG0298,PG0390,PG0035} | 10:10:00
+-- {(UKX),(KJA),(NOZ),(OVB),(MJZ),(CNN)} | {PG0022,PG0352,PG0297,PG0390,PG0036} | 10:10:00
+-- {(UKX),(KJA),(NOZ),(OVB),(MJZ),(CNN)} | {PG0022,PG0351,PG0297,PG0390,PG0036} | 10:10:00
+-- {(UKX),(KJA),(NOZ),(OVB),(MJZ),(CNN)} | {PG0022,PG0352,PG0298,PG0390,PG0036} | 10:10:00
+-- {(UKX),(KJA),(NOZ),(OVB),(MJZ),(CNN)} | {PG0022,PG0351,PG0298,PG0390,PG0036} | 10:10:00
+--(8 rows)
+
+
+-- ФУНКЦИИ И РАСШИРЕНИЯ
+
+
+-- ЗАДАЧА: Найдите расстояние между Калининградом (KGD) и Петропавловском-Камчатским (PKC)
+
+-- необходимо учесть сферическую форму Земли
+
+CREATE EXTENSION IF NOT EXISTS cube;
+CREATE EXTENSION IF NOT EXISTS earthdistance;
+SELECT  round(
+          (a_from.coordinates <@> a_to.coordinates) * 
+          1.609344
+          )
+FROM    airports a_from,
+        airports a_to
+WHERE   a_from.airport_code = 'KGD'
+AND     a_to.airport_code = 'PKC';
+
+-- round 
+---------
+--  7392
+--(1 row)
